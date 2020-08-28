55e3a50685297cfe73ede7d99483e73af76d4c94
syafrin <syafrinibrahim12@gmail.com>
2020-08-28 14:39:27 -0700

    create front end vue-shop

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <CarouselSlider />
+    <ListProduct />
+    <Instagram />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import CarouselSlider from "@/components/CarouselSlider.vue";
+import ListProduct from "@/components/ListProduct.vue";
+import Instagram from "@/components/Instagram.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    CarouselSlider,
+    ListProduct,
+    Instagram,
+    Partner,
+    Footer,
+  },
+};
+</script>
